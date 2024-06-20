ee5afa434940b167f028fbd35b1d74057634a5bc
sergio_gimenez <sergio.gimenez@i2cat.net>
2024-06-20 15:17:00 +0200

    Raise a 404 error if poll not question

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,4 +1,5 @@
 from django.http import HttpResponse
+from django.http import Http404
 from django.shortcuts import render
 from .models import Question
 
@@ -6,12 +7,26 @@ from .models import Question
 def index(request):
     latest_question_list = Question.objects.order_by("-pub_date")[:5]
     context = {"latest_question_list": latest_question_list}
-    return render(request, "polls/index.html", context)
-    # The context is a dictionary mapping template variable names to Python objects.
+    return render(
+        request,
+        "polls/index.html",
+        context
+    )
+    # The context is a dictionary mapping template variable names to
+    # Python objects.
+
+    # The render() function takes the request object as its first argument, a
+    # template name as its second argument and a dictionary as its optional
+    # third argument. It returns an HttpResponse object of the given template
+    # rendered with the given context.
 
 
 def detail(request, question_id):
-    return HttpResponse("You're looking at question %s." % question_id)
+    try:
+        question = Question.objects.get(pk=question_id)
+    except Question.DoesNotExist:
+        raise Http404("Question does not exist")
+    return render(request, "polls/detail.html", {"question": question})
 
 
 def results(request, question_id):
