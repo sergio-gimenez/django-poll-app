9f23b62ac2faf5659429e7fffe1d034475afd72e
sergio_gimenez <sergio.gimenez@i2cat.net>
2024-07-19 11:55:03 +0200

    Create simple model for a blogpost

--- website/main/models.py
+++ website/main/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Post(models.Model):
+    # Id the user gets deleted, then all the posts made by the user will be deleted as well
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self) -> str:
+        return "{}\n{}".format(self.title, self.description)
